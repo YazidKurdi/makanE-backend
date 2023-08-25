ce186073f5ccd152e22dadd0491840a2756a3ee5
Yazid <yazeedkurdi@hotmail.com>
2023-08-25 20:54:32 +0300

    slug instead of name

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 class FoodCategory(models.Model):
     name = models.CharField(max_length=100)
@@ -12,6 +14,11 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     description = models.CharField(max_length=250, blank=True, null=True)
     image = models.ImageField(upload_to='images/', blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)  # Generate slug from the name
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
